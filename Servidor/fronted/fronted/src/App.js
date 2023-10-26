0456d2f18e08577f1d031b425c7e67cb1d9388ed
daferarte <daferarte@gmail.com>
2023-10-26 16:21:08 -0500

    Creacion de formulario con contexto

--- Servidor/fronted/fronted/src/App.js
+++ Servidor/fronted/fronted/src/App.js
@@ -1,32 +1,12 @@
 import React, {useState} from 'react';
-import Button from './Button';
-import Card from './Card';
-
-const theme ={
-  'dark':{
-    backgroundColor:'black',
-    color:'white'
-  },
-  'light':{
-    backgroundColor:'white',
-    color:'black'
-  }
-}
-
-export const ThemeContext = React.createContext();
+import Form from './Form';
 
 
 function App() {
 
-  const [tema, setTheme] = useState(theme.dark);
   return (
     <div>
-      <ThemeContext.Provider value={tema}>
-        <Button/>
-        <Card />
-        <button onClick={()=>setTheme(theme.light)}>Modo claro</button>
-        <button onClick={()=>setTheme(theme.dark)}>Modo oscuro</button>
-      </ThemeContext.Provider>
+      <Form/>
     </div>
   );
 }
