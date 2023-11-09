7d56b0403aaa4bc36c26ed660af3257439872bad
daferarte <daferarte@gmail.com>
2023-11-09 15:50:03 -0500

    reducers, persistor white y black list

--- Servidor/fronted/fronted/src/App.js
+++ Servidor/fronted/fronted/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link, useParams} from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { store } from './store';
 import SignIn from './users/SingIn';
+import { logOut } from './store/user';
+
+let UsuariosOutlet=()=>{
+  let user=useSelector(state=>state.user.user);
+
+  let dispatch=useDispatch();
+  let navigate=useNavigate();
+
+  let doLogOut =()=>{
+    dispatch(
+      logOut()
+    )
+    navigate('/');
+  };
+
+  return(
+    <>
+      {
+        user && <button onClick={doLogOut}>Cerrar Sesion</button>
+      }
+      <Outlet/>
+    </>
+  )
+
+};
 
 let NotImplemented = () => {
   return (
@@ -34,11 +59,11 @@ function App() {
             <Route path="/" element={<NotImplemented/>} />
             <Route path="/login" element={<SignIn/>} />
 
-            <Route path="user" element={<NotImplemented/>}></Route>
-              <Route path='user/register' element={<SignIn/>} />
+            <Route path="user" element={<UsuariosOutlet/>}>
+              <Route path='register' element={<SignIn/>} />
               <Route path='delete/:id' element={<NotImplemented/>} />
               <Route path='update/:id' element={<NotImplemented/>} />
-            
+            </Route>
 
             <Route path="*" element={<Eror404/>} />
           </Routes>
--- Servidor/fronted/fronted/src/store/index.js
+++ Servidor/fronted/fronted/src/store/index.js
@@ -1,9 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer} from 'redux-persist';
 import userSlice from './user'
+import storage from 'redux-persist/lib/storage';
+
+const reducer = combineReducers({
+    user: userSlice
+});
+
+const persistConfig = {
+    key: 'root',
+    storage:storage,
+    whitelist: ['user'],
+    blacklist:[],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
     reducer:{
-        user: userSlice
+        user: persistedReducer
     }
-})
+});
+
+export const persistor = persistStore(store);
